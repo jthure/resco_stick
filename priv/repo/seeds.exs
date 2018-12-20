cf129cd127a918d5d8a523b3eb9ddd7461356aa7
Jonas Thuresson <thuresson.j@gmail.com>
2018-12-20 18:53:23 +0100

    heroku deployment

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,11 +11,11 @@
 # and so on) as they will fail if something goes wrong.
 
 # RescoStick.Users.create_user(%{first_name: "Jonas", last_name: "Thuresson", email: "joth@netcompany.com"})
-jonas = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Jonas", last_name: "Thuresson", email: "joth123@netcompany.com"})
-jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Jakub", last_name: "Bujak", email: "jabu123@netcompany.com"})
-jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Daniel", last_name: "Krysztopa", email: "dkr123@netcompany.com"})
-jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Paulina", last_name: "Stefańczyk", email: "past123@netcompany.com"})
-jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Michał", last_name: "Banaś", email: "mban123@netcompany.com"})
+jonas = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Jonas", last_name: "Thuresson", email: "joth", initials: "joth"})
+jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Jakub", last_name: "Bujak", email: "jabu", initials: "jabu"})
+jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Daniel", last_name: "Krysztopa", email: "dkr", initials: "dkr"})
+jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Paulina", last_name: "Stefańczyk", email: "past", initials: "past"})
+jakub = RescoStick.Repo.insert!(%RescoStick.Users.User{first_name: "Michał", last_name: "Banaś", email: "mban", initials: "mban"})
 RescoStick.Repo.insert!(%RescoStick.Projects.Project{name: "AL-RSSE-FS-Admin"})
 RescoStick.Repo.insert!(%RescoStick.Projects.Project{name: "AL-RSSE-FS-FSE"})
 RescoStick.Repo.insert!(%RescoStick.Projects.Project{name: "AL-RSSE-SC-SCT"})
